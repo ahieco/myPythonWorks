396d5bdd5efaf7e4bf26a0233771b5c7541be871
ahieco <88776797+ahieco@users.noreply.github.com>
2023-03-07 16:46:53 +0800

    Add files via upload

---
+++ no004.py
@@ -0,0 +1,21 @@
+# 输入某年某月某日，判断这一天是这一年的第几天？
+
+
+months = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+year = input("Enter the year(such as: 1955)")
+month = input("Enter the moth(such as: 5 for may)")
+day = input("Enter the day(such as: 3 for day)")
+
+
+leapYear = 0
+if int(year)%4==0 and int(year)%100!=0:
+    leapYear+=1
+else:
+    if int(year)%400==0:
+        leapYear+=1
+
+monthDay = months[:(int(month)-1)]
+
+days = sum(monthDay)+leapYear + int(day)
+print ("the day is no.",days,"days in this year")
---
+++ no005.py
@@ -0,0 +1,7 @@
+aList = []
+for i in range(3):
+    aNum = input("Please input no. %s number: "%(i+1))
+    aList.append(aNum)
+aList.sort()
+
+print(aList)
